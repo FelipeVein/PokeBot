170f08d9f0497e9c57c6dfe1ef2a6ab31a7008f3
Felipe Ferraz <felipe.ferraz@engenharia.ufjf.br>
2021-02-13 21:44:59 -0300

    :bug: Bug fix

--- mark_waypoints.py
+++ mark_waypoints.py
@@ -1,9 +1,10 @@
 import cv2
 import os 
+from copy import deepcopy
 
 PATH = '.\\core\\map\\'
 PATH2 = '.\\core\\mark_waypoints\\'
-MAP_NAME = 'map2'
+MAP_NAME = 'map3'
 
 try:
     os.mkdir(PATH + MAP_NAME)
@@ -16,15 +17,16 @@ counter = 0
 def mark(event,x,y,flags,param):
     global mouseX, mouseY, counter
     if event == cv2.EVENT_LBUTTONDOWN:
-        print('b')
+        print('Waypoint Saved in {}.'.format(PATH + MAP_NAME + '\\{:04d}.png'.format(counter)))
         mouseX,mouseY = x,y
-        small = img[y:y+30, x:x+30, :]
-        cv2.imwrite(PATH + MAP_NAME + '\\{}.png'.format(counter),small)
+        small = img2[y:y+30, x:x+30, :]
+        cv2.imwrite(PATH + MAP_NAME + '\\{:04d}.png'.format(counter),small)
         counter += 1
         cv2.circle(img,(x,y),2,(255,255,255),-1)
     
 
 img = cv2.imread(PATH2 + '{}.png'.format(MAP_NAME))
+img2 = deepcopy(img)
 cv2.namedWindow('Press Q to close')
 cv2.setMouseCallback('Press Q to close',mark)
 
